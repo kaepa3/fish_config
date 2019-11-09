983b73a0882c5a9b92980c7b573a457597e3b634
Takahiro Yamazaki <yamataka@TakahironoMacBook-Pro.local>
2019-11-09 17:26:31 +0900

    add config

---
+++ config.fish
@@ -0,0 +1,11 @@
+# pyenv
+set PATH $HOME/.pyenv/shims $PATH
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin:$PATH
+eval (pyenv init - | source)
+eval (pyenv virtualenv-init - | source)
+# node
+set PATH $PATH:$HOME/.nodebrew/current/bin
+
+alias vi='nvim'
+alias vim='nvim'
