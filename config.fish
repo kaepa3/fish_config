5f903a26122acc6d238102af34a708fdec15b38d
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2020-01-29 00:42:16 +0900

    add

--- config.fish
+++ config.fish
@@ -4,8 +4,11 @@ set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/bin:$PATH
 eval (pyenv init - | source)
 eval (pyenv virtualenv-init - | source)
+
 # node
 set PATH $PATH:$HOME/.nodebrew/current/bin
+
+set PATH ~/.flutterbin/bin $PATH
 
 alias vi='nvim'
 alias vim='nvim'
