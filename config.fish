810ec244bb83ea9736bf82653bc23b16b18fa61e
kaepa3 <kaepa3@gmail.com>
2020-03-16 15:44:21 +0000

    display branch

--- config.fish
+++ config.fish
@@ -11,5 +11,7 @@ set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
 set -x GOPATH (go env GOPATH) 
 set -U fish_user_paths $GOPATH/bin $fish_user_paths   
 
+set -g theme_display_git_master_branch yes
+
 alias vi='nvim'
 alias vim='nvim'
