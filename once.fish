654ab454087dae3ab5335419a75f0f1759026d6f
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2020-03-17 00:42:53 +0900

    path err fix

---
+++ once.fish
@@ -0,0 +1,7 @@
+# -*- mode: fish-mode -*-
+set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths   
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths   
+
+set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths   
+
+set -U fish_user_paths $GOPATH/bin $fish_user_paths   
