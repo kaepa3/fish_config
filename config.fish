a0c16f2568d94d14653ec5bd1fd6ea3e93e9470d
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2024-09-29 23:31:54 +0900

    modified:   config.fish

--- config.fish
+++ config.fish
@@ -4,17 +4,19 @@ set -x PYENV_ROOT $HOME/.pyenv
 eval (pyenv init - | source)
 eval (pyenv virtualenv-init - | source)
 
+# ruby
+eval "$(rbenv init -)"
 
 set -x GOPATH (go env GOPATH) 
-
-set PATH ~/.flutterbin/bin $PATH
-
-set -g theme_display_git_master_branch yes
 
 set -g theme_display_git_master_branch yes
 
 alias vi='nvim'
-alias vim='nvim'
+alias pg='cd ~/Documents/'
+alias op.config='vi ~/.config/fish/config.fish' 
+alias op.nvim.init='vi ~/.config/nvim/init.lua' 
+alias cd.nvim='cd ~/.config/nvim/' 
+alias diclean='docker rmi $(docker images -f "dangling=true" -q)'
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/takahiro/.google-cloud-sdk/path.fish.inc' ]; . '/Users/takahiro/.google-cloud-sdk/path.fish.inc'; end
+# if [ -f '/Users/takahiro/.google-cloud-sdk/path.fish.inc' ]; . '/Users/takahiro/.google-cloud-sdk/path.fish.inc'; end
